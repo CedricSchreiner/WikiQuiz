c14328e29e813b183e6e6a107c9c01fb07ddf13e
weedric <cedi.schreiner@web.de>
2017-08-17 13:15:24 +0200

    a

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { QuizComponent } from './quiz/quiz.component';
+import { TimerbarComponent } from './timerbar/timerbar.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { BackgroundComponent } from './background/background.component';
+import { LoginComponent} from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuizComponent,
+    TimerbarComponent,
+    NavbarComponent,
+    BackgroundComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/timerbar/timerbar.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-timerbar',
+  templateUrl: './timerbar.component.html',
+  styleUrls: ['./timerbar.component.css']
+})
+export class TimerbarComponent {
+}
