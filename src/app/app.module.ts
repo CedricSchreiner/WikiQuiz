26c6883c68c9e4678610b50b3dde3ffb2de66607
Bjoerg <Bjoerg.Schmidt@googlemail.com>
2017-08-22 14:42:03 +0200

    navbar geaendert

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { QuizComponent } from './quiz/quiz.component';
 import { TimerbarComponent } from './timerbar/timerbar.component';
-import { NavbarComponent } from './navbar/navbar.component';
 import { BackgroundComponent } from './background/background.component';
 import { LoginComponent } from './login/login.component';
 import { RouterModule, Routes } from '@angular/router';
@@ -34,7 +33,6 @@ const appRoutes: Routes = [
     AppComponent,
     QuizComponent,
     TimerbarComponent,
-    NavbarComponent,
     BackgroundComponent,
     LoginComponent,
     SignupComponent,
