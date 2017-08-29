8ffa43cdf7ceb09ebad9f4343d2728beda470791
xOnur <Onur_93@yahoo.de>
2017-08-29 15:17:50 +0200

    Results Component Orientation Landscape Style finish

--- src/app/popup/popup.component.ts
+++ src/app/popup/popup.component.ts
@@ -1,10 +1,16 @@
-import { Component} from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-popup',
   templateUrl: './popup.component.html',
   styleUrls: ['./popup.component.css']
 })
-export class PopupComponent {
+export class PopupComponent implements OnInit {
+punkte: string;
+verhaeltnis: string;
 
+ngOnInit() {
+  this.punkte = '400';
+  this.verhaeltnis = '50/100';
+  }
 }
