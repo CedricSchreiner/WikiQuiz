835273fc65c337c6644af4fe3790f92151d98f0e
weedric <cedi.schreiner@web.de>
2017-09-16 18:39:26 +0200

    Nur User die eingeloggt sind können auf die Seite zugreien

--- src/app/static-functions/static.function.ts
+++ src/app/static-functions/static.function.ts
@@ -1,10 +1,10 @@
-import {isNullOrUndefined} from 'util';
+
 
 export function isUserloggedIn(): boolean {
   return (sessionStorage.getItem('email') !== null &&
           sessionStorage.getItem('gamemode') !== null &&
-          sessionStorage.getItem('id') !== null &&
           sessionStorage.getItem('link') !== null &&
+          sessionStorage.getItem('id') !== null &&
           sessionStorage.getItem('password') !== null &&
           sessionStorage.getItem('username') !== null);
 }
