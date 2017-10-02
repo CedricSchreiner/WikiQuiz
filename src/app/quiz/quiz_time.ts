63667b830760204ee763a616361228604f6feda0
xOnur <Onur_93@yahoo.de>
2017-10-02 13:49:38 +0200

    Time Quiz Punkteberechnung --> 100 Punkte für Griffindor pro richtige Antwort.

--- src/app/quiz/quiz_time.ts
+++ src/app/quiz/quiz_time.ts
@@ -64,7 +64,7 @@ export class TimeQuizService extends QuizService {
    * @returns {number}
    */
   public calculatePoints(): number {
-    return 1000;
+      return this.answeredQuestions * 100;
   }
 
   /*Private Section*/
